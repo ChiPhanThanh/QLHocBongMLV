0c976c1f98c0f3add435f33c8808e69f7c436e17
ITPhan <phanthanhchi37@gmail.com>
2022-10-30 15:41:58 +0700

    close application Login

--- QLHocBongMLV/Program.cs
+++ QLHocBongMLV/Program.cs
@@ -16,7 +16,7 @@ namespace QLHocBongMLV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            //Application.Run(new Login());
             //Application.Run(new ResetPassWord());
             //Application.Run(new Sign_in());
            //Application.Run(new panelMain());

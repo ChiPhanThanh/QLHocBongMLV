041c9e256c9d6bb5b306abad55c488e53a87b047
ITPhan <phanthanhchi37@gmail.com>
2022-10-30 14:58:34 +0700

    change button in Sign_in

--- QLHocBongMLV/Sign_in.cs
+++ QLHocBongMLV/Sign_in.cs
@@ -99,17 +99,12 @@ namespace QLHocBongMLV
             {
                     Login login = new Login();
                     login.ShowDialog();
-            DialogResult dr = MessageBox.Show(" Bạn có muốn thoát không", "Thông báo...", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-            if (dr == DialogResult.Yes)
-            {
-                Application.Exit();
             }
             else
             {
                 return;
             }
         }
-
-     
     }
 }
+

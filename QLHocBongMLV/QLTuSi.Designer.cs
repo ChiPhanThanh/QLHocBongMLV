6758c4cc076ff753b01619c30f46378e86bc6fd1
ITPhan <phanthanhchi37@gmail.com>
2022-11-03 22:00:17 +0700

    fix

--- QLHocBongMLV/QLTuSi.Designer.cs
+++ QLHocBongMLV/QLTuSi.Designer.cs
@@ -100,7 +100,8 @@
             // 
             // dataGridViewTS
             // 
-            this.dataGridViewTS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridViewTS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewTS.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(95)))), ((int)(((byte)(153)))));
             this.dataGridViewTS.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
